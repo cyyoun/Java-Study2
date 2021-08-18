58409e37bd822f3ea54aa1da7f3ee616fbd9acfa
cyyoun <cyyoun41@gmail.com>
2021-08-18 13:33:52 +0900

    JAVA입문책공부: 2nd_변수와자료형

---
+++ src/_3rd_연산자/OperationEx.java
@@ -0,0 +1,15 @@
+package _3rd_연산자;
+/* 항과 연산자
+ * Ex) 3+4 에서 3과 4는 항이고 +는 연산자
+연산자는 항의 개수에 따라 단항 연산자, 이항 연산자, 삼항 연산자로 분류
+Ex) 단항 연산자 : ++num;
+	이항 연산자 : num1 + num2;
+	삼항 연산자 : (5>3)?1:0;
+*/
+public class OperationEx {
+
+	public static void main(String[] args) {
+
+	}
+
+}
